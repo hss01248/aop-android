363d0a9f422a4398175e4d43d8094b89a415bcaa
hss01248 <hss012489@gmail.com>
2021-01-11 19:47:36 +0800

    rxjava demo

--- rxjavademo/src/main/java/com/hss01248/rxjavademo/MainActivity.java
+++ rxjavademo/src/main/java/com/hss01248/rxjavademo/MainActivity.java
@@ -1,12 +1,9 @@
-package com.hss01248.aopdemo;
+package com.hss01248.rxjavademo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.hss01248.aopdemo.rxjava.MyFunc;
-import com.hss01248.aopdemo.rxjava.MyObservable;
-import com.hss01248.aopdemo.rxjava.MyObserver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,30 +18,33 @@ public class MainActivity extends AppCompatActivity {
         MyObservable.create(new MyObservable<String>() {
             @Override
             public void subscrib(MyObserver<String> observer) {
+                Log.d("create:", "," + Thread.currentThread().getName());
                 for (int i = 0; i < 10; i++) {
                     observer.onNext(i + "");
                 }
-                Log.d("create:",","+Thread.currentThread().getName());
+
 
             }
         }).map(new MyFunc<Integer, String>() {
             @Override
             public Integer apply(String s) {
-                Log.d("map1:","integer:"+s+","+Thread.currentThread().getName());
+                Log.d("map1:", "integer:" + s + "," + Thread.currentThread().getName());
                 return Integer.parseInt(s) * 2;
             }
-        }).map(new MyFunc<String, Integer>() {
+        }).subscribOnIO()
+        .observerOnMainThread()
+        .map(new MyFunc<String, Integer>() {
             @Override
             public String apply(Integer integer) {
-                Log.d("map2:","integer:"+integer+","+Thread.currentThread().getName());
-                return integer+"->String";
+                Log.d("map2:", "integer:" + integer + "," + Thread.currentThread().getName());
+                return integer + "->String";
             }
         }).subscribOnIO()
         .observerOnMainThread()
         .subscrib(new MyObserver<String>() {
             @Override
             public void onNext(String s) {
-                Log.d("onNext:","finnaly:"+s+","+Thread.currentThread().getName());
+                Log.d("onNext:", "finnaly:" + s + "," + Thread.currentThread().getName());
             }
 
             @Override
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onComplete() {
-                Log.d("onComplete:","finnaly: onComplete");
+                Log.d("onComplete:", "finnaly: onComplete");
             }
         });
 
---
+++ rxjavademo/src/main/java/com/hss01248/rxjavademo/MyFunc.java
@@ -0,0 +1,8 @@
+package com.hss01248.rxjavademo;
+
+public interface MyFunc<R,T> {
+
+    R apply(T t);
+
+
+}
